1f838e4c23c06399a5585d36b233fd994ddd8fa0
chromatus <andzinsan384@gmail.com>
2019-01-12 11:58:33 +0100

    Finishing transition from functions to class and methods.

--- main.py
+++ main.py
@@ -5,84 +5,93 @@ made by Andrzej Klimko
 
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 dataPath = 'salary.csv'
 # location and of .csv file containing data
 
 class Cognitive:
+    """
+    """
+    
+    frame = []
+    complete_data = []
+    partial_data = []
+    X = []
+    y = []
+    to_fill_X = []
+    predicted = []
+    
+    
     def data_loading(self):
-        """loads data from file replacing missing values with NaN."""
-        frame = pd.read_csv(dataPath, na_values = ' ', header = 0)
-        return frame
+        """Loads data from file replacing missing values with NaN."""
+        self.frame = pd.read_csv(dataPath, na_values = ' ', header = 0)
+        return
     
     
-    def data_preprocessing(self, frame):
+    def data_preprocessing(self):
         """Separates complete and incomplete rows into separate frames
         and scales them.
         """
-        complData = frame.dropna()
-        incomplData = frame[frame.isnull().any(axis=1)]
+        self.complete_data = self.frame.dropna()
+        self.partial_data = self.frame[self.frame.isnull().any(axis=1)]
         #splitting complete and incomplete rows
         
-        arr = np.array(complData, dtype = np.float)
-        arr2 = np.array(incomplData, dtype = np.float)
+        arr = np.array(self.complete_data, dtype = np.float)
+        arr2 = np.array(self.partial_data, dtype = np.float)
         # convert values to floats
         arr = MinMaxScaler().fit_transform(arr)
         # scaling complete rows for further processing
-        X, y = arr[:, :-1], arr[:, -1]
-        toFillX = arr2[:, :-1]
+        self.X, self.y = arr[:, :-1], arr[:, -1]
+        self.to_fill_X = arr2[:, :-1]
         # splitting scaled data for further processing
-        toFillX = MinMaxScaler().fit_transform(toFillX)
+        self.to_fill_X = MinMaxScaler().fit_transform(self.to_fill_X)
         # scaling incomplete rows
-        return X, y, toFillX
+        return
     
     
-    def data_predicting(self, X, y, toFillX):
+    def data_predicting(self):
         """Uses sklearn to fill uncompleted rows."""
         from sklearn.svm import SVR
         svr = SVR(kernel='linear')
-        svr.fit(X, y)
-        predy = svr.predict(toFillX)
-        return predy
+        svr.fit(self.X, self.y)
+        self.predicted = svr.predict(self.to_fill_X)
+        return
     
     
-    def data_rescaling(self, X, y, toPred, estData):
+    def data_rescaling(self):
         """Scales the data to what it was before processing."""
-        X = MinMaxScaler(feature_range=(1, 12.7)).fit_transform(X)
-        toPred = MinMaxScaler(feature_range=(2, 12)).fit_transform(toPred)
-        
-        combined = np.concatenate([y, estData])
+        self.X = MinMaxScaler(feature_range=(1, 12.7)).fit_transform(self.X)
+        self.to_fill_X = MinMaxScaler(feature_range=(
+            2, 12)).fit_transform(self.to_fill_X)
+        combined = np.concatenate([self.y, self.predicted])
         combined = np.array(combined, dtype = np.float)
         combined = MinMaxScaler(feature_range=(2750, 13000)).fit_transform(
             combined.reshape(-1, 1))
         # combining data to rescale it
         # done to use data from file for scaling predicted data
-        y, estData = combined[:len(y)], combined[len(y):]
+        self.y, self.predicted = combined[:len(self.y)], combined[len(self.y):]
         #splitting data again, for further use
-        return X, y, toPred, estData
+        return
     
     
-#     def plot(self, X, y, toPred, estData):
-#         # visualizing results
-#         plt.scatter(X, y, label='inputed data')
-#         plt.plot(toPred, estData, color = 'red')
-#         plt.scatter(toPred, estData,
-#             label = 'estimated values', color = 'red')
-#         plt.legend()
-#         plt.show()
-#         return
-# 
-# 
-# # Using created functions
-# dataFrame = dataLoading()
-# globalX, globaly, toPredictX = dataPreprocessing(dataFrame)
-# estimatedData = dataPredicting(globalX, globaly, toPredictX)
-# globalX, globaly, toPredictX, estimatedData = dataRescaling(
-#         globalX, globaly, toPredictX, estimatedData)
-# print("Years worked:  ")
-# print(toPredictX)
-# print("Estimated salary:   ")
-# print(estimatedData)
-# plot(globalX, globaly, toPredictX, estimatedData)
+    def plot(self):
+        """Makes data visualization."""
+        plt.scatter(self.X, self.y, label='inputed data')
+        plt.plot(self.to_fill_X, self.predicted, color = 'red')
+        plt.scatter(self.to_fill_X, self.predicted,
+            label = 'estimated values', color = 'red')
+        plt.legend()
+        plt.show()
+        return
+
+
+# Creating class instance and calling created methods.
+cognitive = Cognitive()
+cognitive.data_loading()
+cognitive.data_preprocessing()
+cognitive.data_predicting()
+cognitive.data_rescaling()
+cognitive.plot()
+
