cfc32adba07f6f1520926d37591e3344094b6427
chromatus <andzinsan384@gmail.com>
2019-01-16 20:13:19 +0100

    README file created, data filtration form added

--- README.txt
+++ README.txt
@@ -1,5 +1,32 @@
+This is application made for Intive Patronage 2019.
 
+It creates very simple django WWW page, working on sqlite3 database. 
+It contains 3 views:
+- main menu - contains only clickable links to other views
+	loaded from template
+- data table presentation - puts all the data from the database
+	into single html table. Only this table and clickable link
+	to the main menu are present on this view.
+	loaded from template
+- data filtration form - simple form with 4 fields to fill:
+	- minimum salary
+	- maximum salary
+	- minimum years worked
+	- maximum years worked
+	Under those fields there are return and continue buttons.
+	After continue button is pressed, the form is sent to 'filtered' view
+- filtered - presents data from the database, that fit the criteria from
+	the form, in html table. Under the table there's clickable link
+	to the main menu.
+- data import - loads data from file, and puts it into the database.
+	Incomplete rows are filled by prediction algorithms, values are 
+	predicted based on data from complete rows. When that is complete
+	confirmation message is displayed.
 
-django superuser
-login: admin
-password: admin
+django superuser:
+	login: admin
+	password: admin
+	
+
+Planned features:
+- filtering data displayed based on the form filled by user
--- patronage/ML/views.py
+++ patronage/ML/views.py
@@ -1,8 +1,10 @@
 from django.template import loader
 from django.http import HttpResponse
+from django.urls import reverse
 
 from .cognitive import Cognitive
 from .models import Salary
+from django.http.response import HttpResponseRedirect
 
 
 def index(request):
@@ -27,3 +29,23 @@ def cognitive(request):
 
 def plots(request):
     return HttpResponse()
+
+def form(request):
+    template = loader.get_template('ML/form.html')
+    salary_list = Salary.objects.order_by('id')
+    context = {'salary_list' : salary_list}
+    return HttpResponse(template.render(context, request))
+
+def data_filtered(request):
+    salary_list = Salary.objects.order_by('id')
+    if request.POST['max_salary'] != '':
+        salary_list = salary_list.exclude(salary__gt=request.POST['max_salary'])
+    if request.POST['min_salary'] != '':
+        salary_list = salary_list.filter(salary__gte=request.POST['min_salary'])
+    if request.POST['max_years'] != '':
+        salary_list = salary_list.exclude(years_worked__gt=request.POST['max_years'])
+    if request.POST['min_years'] != '':
+        salary_list = salary_list.filter(years_worked__gte=request.POST['min_years'])
+    context = {'salary_list' : salary_list}
+    template = loader.get_template('ML/display.html')
+    return HttpResponse(template.render(context, request))
